6876108e07c29c1bea523fdf5c5d6e1b207f5573
Belashab <belashabb@gmail.com>
2022-02-04 13:47:34 +0300

    base files added + axios

---
+++ src/components/header/Header.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Header(){
+    <div className="header-wrapper" key={this.className}>
+        <p className="header-text" key={this.className}>
+            List of passengers
+        </p>
+    </div>
+}
---
+++ src/components/passenger-display-list/PassengerDisplayList.js
@@ -0,0 +1,5 @@
+export default function PassengerDisplayList() {
+    <div className="passenger-display-list" key={this.PassengerDisplayList}>
+        Здесь могла быть Ваша реклама
+    </div>
+}
